0efb4fef7931fc6aeaf27b7ac8cc2244bb2c99ab
Vanish <fijoy5362@gmail.com>
2026-06-02 04:32:17 +0400

    Update Program.cs with file persistence (text and JSON)

--- Program.cs
+++ Program.cs
@@ -1,57 +1,117 @@
-﻿using System;
+using System;
 using LibraryApp;
 
+// Инициализация файлового хранилища
+FileStorage storage = new FileStorage();
+
+// Загрузка данных из файлов при запуске программы
+Console.WriteLine("=== ЗАГРУЗКА ДАННЫХ ===");
 Library library = new Library();
 
-Book book1 = new Book(
-    1,
-    "Война и мир",
-    "Л.Н. Толстой",
-    1869);
+var booksText = storage.LoadBooksFromText();
+var readersText = storage.LoadReadersFromText();
+var issuesText = storage.LoadIssuesFromText();
+var returnsText = storage.LoadReturnsFromText();
 
-Book book2 = new Book(
-    2,
-    "Преступление и наказание",
-    "Ф.М. Достоевский",
-    1866);
+foreach (var book in booksText)
+    library.AddBook(book);
 
-library.AddBook(book1);
-library.AddBook(book2);
+foreach (var reader in readersText)
+    library.AddReader(reader);
 
-Reader reader1 = new Reader(
-    1,
-    "Иван Иванов");
+Console.WriteLine($"Загружено книг (из txt): {booksText.Count}");
+Console.WriteLine($"Загружено читателей (из txt): {readersText.Count}");
+Console.WriteLine();
 
-library.AddReader(reader1);
+// Если данные не загружены, используем тестовые данные
+if (library.GetBooks().Count == 0)
+{
+    Console.WriteLine("=== СОЗДАНИЕ ТЕСТОВЫХ ДАННЫХ ===");
+    
+    Book book1 = new Book(
+        1,
+        "Война и мир",
+        "Л.Н. Толстой",
+        1869);
+
+    Book book2 = new Book(
+        2,
+        "Преступление и наказание",
+        "Ф.М. Достоевский",
+        1866);
+
+    library.AddBook(book1);
+    library.AddBook(book2);
+
+    Reader reader1 = new Reader(
+        1,
+        "Иван Иванов");
+
+    library.AddReader(reader1);
+}
 
 Console.WriteLine("=== ИНФОРМАЦИЯ О КНИГЕ ===");
-Console.WriteLine(book1.GetInfo());
+var firstBook = library.GetBooks().FirstOrDefault();
+if (firstBook != null)
+    Console.WriteLine(firstBook.GetInfo());
 
 Console.WriteLine();
 
 Console.WriteLine("=== ИНФОРМАЦИЯ О ЧИТАТЕЛЕ ===");
-Console.WriteLine(reader1.GetInfo());
+var firstReader = library.GetReaders().FirstOrDefault();
+if (firstReader != null)
+    Console.WriteLine(firstReader.GetInfo());
 
 Console.WriteLine();
 
 Console.WriteLine("=== ВЫДАЧА КНИГИ ===");
-BookIssue issue = library.IssueBook(1, 1);
-Console.WriteLine(issue.GetInfo());
+if (firstBook != null && firstReader != null)
+{
+    BookIssue issue = library.IssueBook(firstBook.Id, firstReader.Id);
+    Console.WriteLine(issue.GetInfo());
+}
 
 Console.WriteLine();
 
 Console.WriteLine("=== СОСТОЯНИЕ КНИГИ ===");
-Console.WriteLine(book1.GetInfo());
+if (firstBook != null)
+    Console.WriteLine(firstBook.GetInfo());
 
 Console.WriteLine();
 
 Console.WriteLine("=== ВОЗВРАТ КНИГИ ===");
-BookReturn returnedBook = library.ReturnBook(1, 1);
-Console.WriteLine(returnedBook.GetInfo());
+if (firstBook != null && firstReader != null)
+{
+    BookReturn returnedBook = library.ReturnBook(firstBook.Id, firstReader.Id);
+    Console.WriteLine(returnedBook.GetInfo());
+}
 
 Console.WriteLine();
 
 Console.WriteLine("=== СОСТОЯНИЕ КНИГИ ПОСЛЕ ВОЗВРАТА ===");
-Console.WriteLine(book1.GetInfo());
+if (firstBook != null)
+    Console.WriteLine(firstBook.GetInfo());
 
+Console.WriteLine();
+
+// Сохранение данных в текстовые файлы при выходе
+Console.WriteLine("=== СОХРАНЕНИЕ ДАННЫХ В ТЕКСТОВЫЕ ФАЙЛЫ ===");
+storage.SaveBooksToText(library.GetBooks());
+storage.SaveReadersToText(library.GetReaders());
+storage.SaveIssuesToText(library.GetIssues());
+storage.SaveReturnsToText(library.GetReturns());
+Console.WriteLine("Данные сохранены в текстовые файлы");
+
+Console.WriteLine();
+
+// Сохранение данных в JSON файлы
+Console.WriteLine("=== СОХРАНЕНИЕ ДАННЫХ В JSON ФАЙЛЫ ===");
+storage.SaveBooksToJson(library.GetBooks());
+storage.SaveReadersToJson(library.GetReaders());
+storage.SaveIssuesToJson(library.GetIssues());
+storage.SaveReturnsToJson(library.GetReturns());
+Console.WriteLine("Данные сохранены в JSON файлы");
+
+Console.WriteLine();
+Console.WriteLine("Нажмите любую клавишу для выхода...");
 Console.ReadKey();
